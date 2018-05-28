79239b5dc9525bbec44285f9279d9e982e3b36b8
youkunhuang <youkunhuang@tencent.com>
2018-05-28 14:51:26 +0800

    chore(runtime): ajax-parse-error
    parse error不再算入考核

--- bin/tsw/ajax/ajax.js
+++ bin/tsw/ajax/ajax.js
@@ -1072,7 +1072,6 @@ Ajax.prototype.doRequest = function(opt) {
 
                         require('util/mail/mail.js').SendMail(key, 'js data', 1800, {
                             'Title'            : key,
-                            'runtimeType'    : 'ParseError',
                             'Content'        : Content,
                             'MsgInfo'        : '错误堆栈:\n' + parseErr.stack
                         });
