675d904779cebb9aa1be763a658fc4245952fa8a
RobinzZH <choco-love@qq.com>
2018-06-14 16:25:59 +0800

    refactor(http.test): code refactor

--- test/bin/tsw/util/isTST.test.js
+++ test/bin/tsw/util/isTST.test.js
@@ -13,39 +13,26 @@ describe('test isTST', () => {
     const req = {};
 
     it('no extendMod', async () => {
-
-
         config.extendMod = false;
-
         expect(isTST.isTST(req)).to.equal(false);
-
     });
 
     it('extendMod no isTST', async () => {
-
         config.extendMod = {};
-
         expect(isTST.isTST(req)).to.equal(false);
-
     });
 
     it('extendMod isTST return false', async () => {
-
         config.extendMod = {
             isTST: () => false
         };
-
         expect(isTST.isTST(req)).to.equal(false);
-
     });
 
     it('extendMod isTST return true', async () => {
-
         config.extendMod = {
             isTST: () => true
         };
-
         expect(isTST.isTST(req)).to.equal(true);
-
     });
 });
