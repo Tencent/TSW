1452ec01c56b89fb103e1bf3eeb642874a54bc7d
lemanzhang <lemanzhang@tencent.com>
2018-06-27 12:15:30 +0800

    chore(config): allows to specify the path of config at startup

--- conf/config.js
+++ conf/config.js
@@ -1,4 +1,18 @@
-module.exports = require('../examples/framework/config.js');
+/* !
+ * Tencent is pleased to support the open source community by making Tencent Server Web available.
+ * Copyright (C) 2018 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+'use strict';
 
-//module.exports = require('../examples/skyMode/config.js');
+const processArgs = plug('util/process.args.js');
 
+let configPath = '../examples/framework/config.js';
+
+if (typeof processArgs.config === 'string') {
+    configPath = processArgs.config;
+}
+
+module.exports = require(configPath);
