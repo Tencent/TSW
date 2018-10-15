192fd21b4299ef141863c0334f7bd5cb53b8c4ff
kingweicai <kingweicai@tencent.com>
2018-10-15 12:33:26 +0800

    fix: remove other commits

--- bin/proxy/websocket.js
+++ bin/proxy/websocket.js
@@ -277,14 +277,12 @@ exports.start_listen = function() {
     const ws = new WSServer({
         server: global.TSW_HTTP_SERVER
     });
-    global.TSW_WS_SERVER = ws;
     bind_listen(ws);
 
     if (global.TSW_HTTPS_SERVER) {
         const wss = new WSServer({
             server: global.TSW_HTTPS_SERVER
         });
-        global.TSW_WSS_SERVER = ws;
         bind_listen(wss);
     }
 };
