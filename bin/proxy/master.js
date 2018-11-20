209fbd0173177cae9d176697b3af5ed75c58e994
loviselu <loviselu@tencent.com>
2018-11-20 15:15:28 +0800

    feat(master-monitor): restart tsw when master is not reply

--- bin/proxy/master.js
+++ bin/proxy/master.js
@@ -23,6 +23,8 @@ const cpuMap = [];
 const tnm2 = require('api/tnm2');
 const network = require('util/network.js');
 const serverInfo = require('serverInfo.js');
+const { fork } = require('child_process');
+const path = require('path');
 
 process.on('uncaughtException', function(e) {
 
@@ -129,6 +131,7 @@ function startServer() {
         masterEventHandler();
         checkWorkerAlive();
         startAdmin();
+        startMasterMonitor();
 
         Object.defineProperty(process, 'title', Object.assign(Object.create(null), {
             value: 'TSW',
@@ -511,4 +514,14 @@ function masterEventHandler() {
             }
         }
     });
+}
+
+function startMasterMonitor() {
+
+    logger.info('start master monitor....');
+
+    fork(path.resolve(__dirname, './master-monitor.js'), [process.pid], {
+        silent: false
+    });
+
 }
