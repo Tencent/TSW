cba54dcdcc99c3d44720db77136b549a0fbab434
mapleeit <522856232@qq.com>
2019-11-05 21:13:44 +0800

    chore: update package.json

--- test/core/add.test.ts
+++ test/core/add.test.ts
@@ -1,6 +1,6 @@
 import * as assert from 'assert'
 
-import { add } from '../../src/core/add'
+import { add } from '../../lib/core/add'
 
 describe('test add', function() {
     it('add(1,2)=3', function(done) {
