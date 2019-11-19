bdc4e50c22e69c1bc0118874b75b23818d4ede09
mapleeit <522856232@qq.com>
2019-11-19 20:25:40 +0800

    test(console.hack): add more console.hack test

--- lib/core/__test__/runtime/console.hack.test.ts
+++ lib/core/__test__/runtime/console.hack.test.ts
@@ -1,10 +1,17 @@
-import { consoleHack } from "../../runtime/console.hack";
+import { consoleHack, consoleRestore } from "../../runtime/console.hack";
+import logger from "../../logger/index";
 
-// jest.mock('../../logger/index')
+jest.mock('../../logger/index')
+
+const mockedWriteLog = logger.writeLog as jest.Mock<typeof logger.writeLog>
 
 beforeAll(() => {
   consoleHack();
 });
+
+afterAll(() => {
+  consoleRestore();
+})
 
 describe("console hack test", () => {
   test("ensure console contains all origin functions", () => {
@@ -17,11 +24,18 @@ describe("console hack test", () => {
   });
 
   test("console.debug should be logged by logger", () => {
+    expect(mockedWriteLog.mock.calls.length).toEqual(0);
     console.debug("test_log");
+    expect(mockedWriteLog.mock.calls.length).toEqual(1);
     console.log("test_log");
+    expect(mockedWriteLog.mock.calls.length).toEqual(2);
     console.info("test_log");
+    expect(mockedWriteLog.mock.calls.length).toEqual(3);
     console.dir("test_log");
+    expect(mockedWriteLog.mock.calls.length).toEqual(4);
     console.warn("test_log");
+    expect(mockedWriteLog.mock.calls.length).toEqual(5);
     console.error("test_log");
+    expect(mockedWriteLog.mock.calls.length).toEqual(6);
   });
 });
