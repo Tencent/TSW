69b3c78c1fcaa914e282052e189899a4b21997fc
mapleeit <522856232@qq.com>
2019-12-19 17:12:39 +0800

    refactor(capture): a better code structure

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,13 @@
 import { consoleHack } from "./core/runtime/console.hack";
 import { httpCreateServerHack } from "./core/runtime/create-server.hack";
 import { dnsHack } from "./core/runtime/dns.hack";
+import { requestHack } from "./core/runtime/capture/index";
 
 export default async (absolutePath2Main: string): Promise<void> => {
   httpCreateServerHack();
   dnsHack();
   consoleHack();
+  requestHack();
 
   await import(absolutePath2Main);
 };
