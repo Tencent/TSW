bfd8d9ab49091e915338df80ee5b3adb71eafc9e
mapleeit <522856232@qq.com>
2019-12-26 14:53:09 +0800

    feat(capture): add current request capturing

--- lib/core/runtime/create-server.hack.ts
+++ lib/core/runtime/create-server.hack.ts
@@ -8,10 +8,23 @@
 
 import * as http from "http";
 import * as domain from "domain";
+import { RequestLog } from "../context";
+import { address } from "ip";
+import { AddressInfo } from "net";
 import { captureOutgoing } from "./capture/outgoing";
+import { captureIncoming } from "./capture/incoming";
+import { eventBus, EVENT_LIST, EventPayload } from "../bus";
 
 let httpCreateServerHacked = false;
 let originHttpCreateServer = null;
+
+export interface ResponseEventPayload extends EventPayload {
+  data: {
+    req: http.IncomingMessage;
+    res: http.ServerResponse;
+    currentRequest: RequestLog | undefined;
+  };
+}
 
 export const httpCreateServerHack = (): void => {
   if (!httpCreateServerHacked) {
@@ -37,6 +50,16 @@ export const httpCreateServerHack = (): void => {
       }
 
       const requestListenerWrap: http.RequestListener = (req, res) => {
+        const start = new Date();
+        const timestamps: RequestLog["timestamps"] = {
+          dnsTime: 0,
+          requestStart: start,
+          onSocket: start,
+          onLookUp: start,
+          requestFinish: start,
+          socketConnect: start
+        } as RequestLog["timestamps"];
+
         console.log("httpCreateServerHack - creating domain");
 
         const d = domain.create();
@@ -50,6 +73,113 @@ export const httpCreateServerHack = (): void => {
 
           console.log(process.domain);
           requestListener(req, res);
+        });
+
+        const requestInfo = captureIncoming(req);
+
+        res.writeHead = ((fn): typeof res.writeHead => (
+          ...args: unknown[]
+        ): ReturnType<typeof res.writeHead> => {
+          timestamps.onResponse = new Date();
+
+          const payload: ResponseEventPayload = {
+            code: 0,
+            msg: "success",
+            success: true,
+            error: null,
+            data: {
+              req, res, currentRequest: undefined
+            }
+          };
+
+          eventBus.emit(EVENT_LIST.RESPONSE_START, payload);
+
+          captureOutgoing(res);
+
+          return fn.apply(res, args);
+        })(res.writeHead);
+
+        res.once("finish", () => {
+          const context = process.domain.currentContext;
+
+          context.currentRequest = {
+            SN: context.SN,
+
+            protocol: "HTTP",
+            host: req.headers.host,
+            url: `http://${req.headers.host}${req.url}`,
+            cache: "",
+            process: `TSW: ${process.pid}`,
+            resultCode: res.statusCode,
+            contentLength: Number(res.getHeader("content-length")),
+            contentType: res.getHeader("content-type"),
+            clientIp: req.socket.remoteAddress,
+            clientPort: req.socket.remotePort,
+            serverIp: address(),
+            serverPort: (req.socket.address() as AddressInfo).port,
+            requestHeader: ((): string => {
+              const result = [];
+              result.push(`${req.method} ${
+                req.url} HTTP/${req.httpVersion}`);
+
+              Object.keys(req.headers).forEach((key) => {
+                result.push(`${key}: ${req.headers[key]}`);
+              });
+
+              result.push("");
+              result.push("");
+
+              return result.join("\r\n");
+            })(),
+            requestBody: requestInfo.body.toString("base64"),
+            responseHeader: ((): string => {
+              const result = [];
+              result.push(`HTTP/${req.httpVersion} ${
+                res.statusCode} ${res.statusMessage}`);
+
+              const resHeaders = res.getHeaders();
+              Object.keys(resHeaders).forEach((key) => {
+                result.push(`${key}: ${resHeaders[key]}`);
+              });
+
+              result.push("");
+              result.push("");
+
+              return result.join("\r\n");
+            })(),
+            responseBody: (res as any)._body.toString("base64"),
+            timestamps
+          } as RequestLog;
+
+          const payload: ResponseEventPayload = {
+            code: 0,
+            msg: "success",
+            success: true,
+            error: null,
+            data: {
+              req, res, currentRequest: context.currentRequest as RequestLog
+            }
+          };
+
+          eventBus.emit(EVENT_LIST.RESPONSE_FINISH, payload);
+        });
+
+        res.once("close", () => {
+          timestamps.responseClose = new Date();
+
+          const context = process.domain.currentContext;
+
+          const payload: ResponseEventPayload = {
+            code: 0,
+            msg: "success",
+            success: true,
+            error: null,
+            data: {
+              req, res, currentRequest: context.currentRequest as RequestLog
+            }
+          };
+
+          eventBus.emit(EVENT_LIST.RESPONSE_CLOSE, payload);
         });
       };
 
