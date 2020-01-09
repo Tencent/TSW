67d4a9d271e05a519314c0eb6bf8efe406b56bcb
mapleeit <522856232@qq.com>
2020-01-09 20:07:23 +0800

    docs(readme): add quick start and some examples

---
+++ examples/koa/koa.js
@@ -0,0 +1,29 @@
+const Koa = require("koa");
+const axios = require("axios");
+
+const app = new Koa();
+
+app.use(async ctx => {
+  await axios.get(
+    "http://jsonplaceholder.typicode.com/todos/1"
+  ).then(res => {
+    console.log(res.data);
+  });
+
+  await axios.post("http://jsonplaceholder.typicode.com/posts", {
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  }).then(res => {
+    console.log(res.data);
+  });
+
+
+  ctx.body = "Hello, tsw 2.0";
+  ctx.status = 200;
+}).listen(4443);
