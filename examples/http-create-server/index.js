6f5605fee53554130e2217a5550943a6c19613fc
Zack921 <407473490@qq.com>
2020-02-19 21:42:59 +0800

    fix: add default tsw.config

--- examples/http-create-server/index.js
+++ examples/http-create-server/index.js
@@ -4,5 +4,6 @@ console.log("try to start a server");
 
 http.createServer((req, res) => {
   res.status = 200;
-  console.log(req.url)
+  console.log(req.url);
+  res.end('Hello World!');
 }).listen(4443);
