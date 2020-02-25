4568a4eab4eb57122721d725d35bf72e7a05a779
Zack921 <407473490@qq.com>
2020-02-25 19:45:46 +0800

    fix(isinspect): allow inspecting in default port

--- lib/core/util/isInspect.ts
+++ lib/core/util/isInspect.ts
@@ -1,8 +1,8 @@
 const isInspect = (): boolean => {
   const nodeOptions = process.env.NODE_OPTIONS;
   return Boolean(nodeOptions && (
-    nodeOptions.includes("--inspect=")
-    || nodeOptions.includes("--inspect-brk=")));
+    nodeOptions.includes("--inspect")
+    || nodeOptions.includes("--inspect-brk")));
 };
 
 export default isInspect;
