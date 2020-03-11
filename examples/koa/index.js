65eaf12575d0075bfa6b6ec6454ef95bcda9b512
mapleeit <522856232@qq.com>
2020-03-11 19:02:40 +0800

    refactor(example): update koa tswconfig

--- examples/koa/index.js
+++ examples/koa/index.js
@@ -4,24 +4,24 @@ const axios = require("axios");
 const app = new Koa();
 
 app.use(async ctx => {
-  await axios.get(
-    "http://jsonplaceholder.typicode.com/todos/1"
-  ).then(res => {
-    console.log(res.data);
-  });
+  // await axios.get(
+  //   "http://jsonplaceholder.typicode.com/todos/1"
+  // ).then(res => {
+  //   console.log(res.data);
+  // });
 
-  await axios.post("http://jsonplaceholder.typicode.com/posts", {
-    body: JSON.stringify({
-      title: 'foo',
-      body: 'bar',
-      userId: 1
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  }).then(res => {
-    console.log(res.data);
-  });
+  // await axios.post("http://jsonplaceholder.typicode.com/posts", {
+  //   body: JSON.stringify({
+  //     title: 'foo',
+  //     body: 'bar',
+  //     userId: 1
+  //   }),
+  //   headers: {
+  //     "Content-type": "application/json; charset=UTF-8"
+  //   }
+  // }).then(res => {
+  //   console.log(res.data);
+  // });
 
 
   ctx.body = "Hello, tsw 2.0";
--- examples/koa/tswconfig.js
+++ examples/koa/tswconfig.js
@@ -5,34 +5,21 @@ module.exports = {
     new OpenPlatformPlugin({
       appid: "tsw1431",
       appkey: "PwPaD4RRAsrSdRZjQSc3fbKM",
-      getReportList: () => {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve(["demoUser"]);
-          }, 2000)
-        });
-      },
+      reportStrategy: "proxied",
       // 只支持同步写法
       getUid: (request) => {
-        const cookie = request.headers.cookie;
-
-        if (!cookie) return;
-
-        const uid = cookie.split("quid=")[1].split(";")[0];
-        return uid;
+        return request.headers["uid"];
       },
       getProxyInfo: () => {
         return {
-          "127.0.0.1": {
-            "port": 80,
-            "name": "2.0demo",
-            "group": "TSW",
-            "groupName": "TSW团队",
-            "desc": "2.0demo测试环境",
-            "order": 30,
-            "owner": "demoUser",
-            "alphaList": ["demoUser"]
-          }
+          "port": 80,
+          "name": "2.0demo",
+          "group": "TSW",
+          "groupName": "TSW团队",
+          "desc": "2.0demo测试环境",
+          "order": 30,
+          "owner": "demoUser",
+          "alphaList": ["demoUser"]
         };
       }
     })
