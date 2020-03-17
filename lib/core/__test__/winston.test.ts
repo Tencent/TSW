e51e092db13334411f100ce1064ae70e3e05fbcd
aiyanayu <aiyanayu@tencent.com>
2020-03-17 17:32:11 +0800

    test(winston): fix test error

--- lib/core/__test__/winston.test.ts
+++ lib/core/__test__/winston.test.ts
@@ -1,14 +1,29 @@
+import * as winston from "winston";
 import { winstonHack } from "../winston";
 import logger from "../../core/logger/index";
+
+global.tswConfig = {
+  plugins: [],
+  winstonTransports: undefined
+};
 
 describe("test winstonHack env", () => {
   test("test logger's winstonLogger when it hasn't hacked", () => {
     expect(logger.winstonLogger).toBe(undefined);
   });
 
-  global.tswConfig.winstonTransports = undefined;
-  winstonHack();
   test("test winstonHack while config without winstonTransports ", () => {
-    expect(logger.winstonLogger.log).toBe(undefined);
+    winstonHack();
+    expect(logger.winstonLogger).toBe(undefined);
+  });
+
+
+  test("test winstonHack while config with Console Transports ", () => {
+    global.tswConfig.winstonTransports = [
+      new winston.transports.Console()
+    ];
+
+    winstonHack();
+    expect(logger.winstonLogger.transports.length).toBe(1);
   });
 });
