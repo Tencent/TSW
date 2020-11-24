736620efd57a6f1e550db123c8265d2b55d01a9e
mapleeit <522856232@qq.com>
2020-11-24 16:02:22 +0800

    test: add collectCoverageFrom for jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   testEnvironment: "node",
   coverageDirectory: "coverage",
   coverageReporters: ["json", "lcov", "text"],
+  collectCoverageFrom: [
+    "lib/**/*.ts"
+  ],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"]
 };
