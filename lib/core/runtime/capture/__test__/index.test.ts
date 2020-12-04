1f534c0a2ce6a0680fcdd0ad02bdde73c91818f1
mapleeit <522856232@qq.com>
2020-12-04 16:49:23 +0800

    test: add test for capture/index.ts

---
+++ lib/core/runtime/capture/__test__/index.test.ts
@@ -0,0 +1,41 @@
+import * as http from "http";
+import { requestHack, requestRestore } from "../index";
+
+/**
+ * 4000 - 5000 random port
+ */
+const randomPort = (): number => Math.floor(Math.random() * 1000 + 4000);
+
+let server: http.Server;
+let port: number;
+
+const RESPONSE_STRING = "success";
+
+beforeAll(() => {
+  requestHack();
+
+  port = randomPort();
+  server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.end(RESPONSE_STRING);
+  }).listen(port);
+});
+
+afterAll(() => {
+  requestRestore();
+
+  server.close();
+});
+
+describe("capture(request hack) test", () => {
+  test("http request", async () => {
+    await new Promise((resolve) => {
+      http.request(`http://127.0.0.1:${port}`, (res) => {
+        res.on("data", (d) => {
+          expect(d.toString("utf8")).toBe(RESPONSE_STRING);
+          resolve();
+        });
+      }).end();
+    });
+  });
+});
